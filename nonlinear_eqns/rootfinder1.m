b202f1a9a0af7fa08feff33488f9a2fb460c024b
Alec Sblendorio <alecsblendorio@Alecs-MacBook-Air.local>
2020-10-13 09:56:38 -0400

    Problem2  work

---
+++ nonlinear_eqns/rootfinder1.m
@@ -0,0 +1,5 @@
+%% Problem 2 (a)
+%%Suppose you have a polynomial of known or given order and need to find all of its roots.
+%%Write a block of code or a script that uses the exact Newton method 
+
+
