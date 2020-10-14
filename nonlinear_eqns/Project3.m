8865cab44b3bd6a29cf9d63a7b09638e5bda5daa
Alec Sblendorio <alecsblendorio@Alecs-MacBook-Air.local>
2020-10-14 11:32:50 -0400

    Project3 updates1

--- nonlinear_eqns/Project3.m
+++ nonlinear_eqns/Project3.m
@@ -24,10 +24,67 @@
 %%look them up and verify your solutions (cite your sources).
 
 disp('%%%%%%%%%%%%%%%%%%PROBLEM #1 ANSWER BEGIN%%%%%%%%%%%%%%%%%%');
-disp('%%%%%%%%Part A, B, C Solution:%%%%%%%');
-soln = @Newton_Rhapson; 
-x = soln();
-disp('%%%%%%%%End Part A, B, C Solution:%%%%%%%');
+disp('%%%%%%%%Part A Solution:%%%%%%%');
+
+maxit=100;       %maximum number of iterations allowed
+minx=0;
+maxx=2*pi;
+tol=1e-9;        %how close to zero we need to get to cease iterations
+
+
+%% Objective function defs.
+f=@objfun2;      %set the function for which we are finding roots, change to illustrate different problems
+fprime=@objfun2_deriv;
+x=linspace(minx,maxx,64);   %grid for basic plotting purposes
+ygrid=f(x);
+verbose=true;
+
+
+%% Plot the function we are finding roots for
+figure(1);
+plot(x,ygrid);
+title('Objective function')
+xlabel('x')
+ylabel('y')
+axis tight;
+
+
+%% Newton-Rhapson root-finding method
+verbose=true;
+[xNewton,itNew,flag]=newton_approx(f,-0.3*i,100,tol,verbose);
+disp('Root value through Newton method:  ');
+disp(xNewton);
+disp('Number of iterations required to reach tolerance:  ');
+disp(itNew);
+
+[xNewton,itNew,flag]=newton_approx(f,0.3*i,100,tol,verbose);
+disp('Root value through Newton method:  ');
+disp(xNewton);
+disp('Number of iterations required to reach tolerance:  ');
+disp(itNew);
+
+
+% %% Newton approach for suspected complex roots
+disp('Complex Stuff');
+[xNewton,itNew]=newton_approx(f,7*i,100,tol,verbose);
+disp('Root value through Newton method:  ');
+disp(xNewton);
+disp('Number of iterations required to reach tolerance:  ');
+disp(itNew);
+
+[xNewton,itNew]=newton_approx(f,-7*i,100,tol,verbose);
+disp('Root value through Newton method:  ');
+disp(xNewton);
+disp('Number of iterations required to reach tolerance:  ');
+disp(itNew);
+
+disp('%%%%%%%%End Part A Solution:%%%%%%%');
+
+
+disp('%%%%%%%%Part B Solution:%%%%%%%');
+
+
+disp('%%%%%%%%End Part B Solution:%%%%%%%');
 
 disp('%%%%%%%%%%%%%%%%%%PROBLEM #1 ANSWER END%%%%%%%%%%%%%%%%%%');
 %% Problem #2: Numerical solution for multiple polynomial roots
@@ -60,8 +117,49 @@ disp('%%%%%%%%%%%%%%%%%%PROBLEM #3 ANSWER BEGIN%%%%%%%%%%%%%%%%%%');
 %%(given the in course repository: newton2D exact.m) to find all four roots of the system
 
 disp('%%%%%%%%Part A Solution:%%%%%%%');
-soln5 = @Newton_Rhapson2DAS;
-s = soln5();
+%% Multidimensional function from Problem #3a
+fm=@objfun2Df_AS;
+gm=@objfun2Dg_AS;
+gradfm=@grad_objfun2Df_AS;
+gradgm=@grad_objfun2Dg_AS;
+
+%this is for plotting
+x=linspace(-1.5,1.5,20);
+y=linspace(-1.5,1.5,20);
+[X,Y]=meshgrid(x,y);
+F=fm(X,Y);
+G=gm(X,Y);
+
+%% Newton's method for multi-variable nonlinear equations
+%x0=i;
+%y0=0.258*i;
+% use for loop to iterate over inital x0 and y0 values
+%loop keep stopping after finding 1 value, need to keep it going to find
+%all 4 roots
+j=1;
+for i=-2.5:0.25:2.5
+    x0=i;
+    y0=i-0.4;
+    [xm,ym,it2D,success2D]=newton2D_exactAS(fm,gradfm,gm,gradgm,x0,y0,100,1e-6,true);
+%     finalarray(1,j)=xm;
+%     finalarray(2,j)=ym;
+    j=j+1;
+end %for
+
+disp('Solution');
+%disp(finalarray);
+disp(xm);
+disp(ym);
+% disp(xm);
+% disp(ym);
+% disp(it2D);
+% disp(success2D);
+
+figure;
+surf(X,Y,F);
+hold on;
+surf(X,Y,G);
+plot3(xm,ym,0,'wo','MarkerSize',32,'LineWidth',8);
 
 disp('%%%%%%%%End Part A Solution:%%%%%%%');
 
@@ -69,6 +167,53 @@ disp('%%%%%%%%End Part A Solution:%%%%%%%');
 %%to find a root for the three equations system
 
 disp('%%%%%%%%Part B Solution:%%%%%%%');
+%% Multidimensional function from Problem #3
+%%Part 3b of problem, compute the roots of 3 equations (3 dimensions)
+fm=@objfun3Df_AS;
+gm=@objfun3Dg_AS;
+km=@objfun3Dk_AS;
+gradfm=@grad_objfun3Df_AS;
+gradgm=@grad_objfun3Dg_AS;
+gradkm=@grad_objfun3Dk_AS;
+%this is for plotting
+x=linspace(-1.5,1.5,20);
+y=linspace(-1.5,1.5,20);
+z=linspace(-1.5,1.5,20);
+[X,Y,Z]=meshgrid(x,y,z);
+F=fm(X,Y,Z);
+G=gm(X,Y,Z);
+K=km(X,Y,Z);
+
+%% Newton's method for multi-variable nonlinear equations
+%x0=i;
+%y0=0.258*i;
+%z0=0.58*i;
+x0=0.5;
+y0=0.5;
+z0=0.5;
+[xm,ym,zm,it3D,success3D]=newton3D_exactAS(fm,gradfm,gm,gradgm,km,gradkm,x0,y0,z0,100,1e-6,true);
+
+disp('%%%%%Roots for All 3 Equations%%%%%%');
+disp('X-Root');
+disp(xm);
+disp('Y-Root');
+disp(ym);
+disp('Z-Root');
+disp(zm);
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+%%Root Checker:
+disp('This section evaluates the roots and checks if the equations go to Zero:');
+r1=fm(xm,ym,zm);
+disp('When plugged back into Equation #1: x^2+y^2+z^2 = 6');
+disp(r1);
+r2=gm(xm,ym,zm);
+disp('When plugged back into Equation #2: x^2−y^2+2*z^2 = 2');
+disp(r2);
+r3=km(xm,ym,zm);
+disp('When plugged back into Equation #3: 2*x^2+y^2−z^2 = 3');
+disp(r3);
+disp('The values shown above are not exactly Zero, but they are quit close. This is due to precision and rounding in matlab.');
+
 
 disp('%%%%%%%% End Part B Solution:%%%%%%%');
 
