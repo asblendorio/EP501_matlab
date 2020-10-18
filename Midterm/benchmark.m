3aa0a604469f3cb7b3f3bdbe364b81ca6e678bd5
Alec Sblendorio <alecsblendorio@Alecs-MacBook-Air.local>
2020-10-18 12:22:15 -0400

    Midterm Problem1 partb9

--- Midterm/benchmark.m
+++ Midterm/benchmark.m
@@ -12,27 +12,27 @@ nvals=25:25:250;
 testtimes=zeros(size(nvals));
 lrep=10;     %how many times to repeat each test
 
-disp('Start of tests of Gaussian-elimination scaling');
-for in=1:numel(nvals)
-    nlarge=nvals(in);
-    Blarge=diag(-1*ones(nlarge-1,1),-1)+diag(-1*ones(nlarge-1,1),1)+diag(4*ones(nlarge,1),0);    %this must be diagonally dominant or else the method won't converge
-    blarge=ones(nlarge,1);
-    
-    for irep=1:lrep     %benchmark will repeat the same solution several times to eliminate random variations from CPU load, etc.
-        tstart=cputime;
-        [Blargemod,ordlarge]=Gauss_elim(Blarge,blarge);
-        xlarge=backsub(Blargemod(ordlarge,:));
-        tend=cputime;
-        testtimes(in)=testtimes(in)+(tend-tstart)/lrep;
-    end %for
-    disp([' GE solution for system of size ',num2str(nlarge),' takes average time ',num2str(testtimes(in)),' s']);
-end %for
-
-figure(1);
-plot(nvals,testtimes,'o','LineWidth',2,'MarkerSize',20,'MarkerFaceColor','blue')
-xlabel('system size');
-ylabel('time to solve (s)');
-title('Empirically Determined Performance');
+% disp('Start of tests of Gaussian-elimination scaling');
+% for in=1:numel(nvals)
+%     nlarge=nvals(in);
+%     Blarge=diag(-1*ones(nlarge-1,1),-1)+diag(-1*ones(nlarge-1,1),1)+diag(4*ones(nlarge,1),0);    %this must be diagonally dominant or else the method won't converge
+%     blarge=ones(nlarge,1);
+%     
+%     for irep=1:lrep     %benchmark will repeat the same solution several times to eliminate random variations from CPU load, etc.
+%         tstart=cputime;
+%         [Blargemod,ordlarge]=Gauss_elim(Blarge,blarge);
+%         xlarge=backsub(Blargemod(ordlarge,:));
+%         tend=cputime;
+%         testtimes(in)=testtimes(in)+(tend-tstart)/lrep;
+%     end %for
+%     disp([' GE solution for system of size ',num2str(nlarge),' takes average time ',num2str(testtimes(in)),' s']);
+% end %for
+% 
+% figure(1);
+% plot(nvals,testtimes,'o','LineWidth',2,'MarkerSize',20,'MarkerFaceColor','blue')
+% xlabel('system size');
+% ylabel('time to solve (s)');
+% title('Empirically Determined Performance');
 
 disp('Start of tests for Jacobi iteration');
 tol=1e-9;
@@ -60,7 +60,7 @@ ylabel('time to solve (s)');
 legend('Gauss elim.','Jacobi it.')
 title('Empirically Determined Performance');
 
-disp('Start of tests of Tridiagonal');
+disp('Start of tests for Tridiagonal');
 for in=1:numel(nvals)
     nlarge=nvals(in);
     Blarge=diag(-1*ones(nlarge-1,1),-1)+diag(-1*ones(nlarge-1,1),1)+diag(4*ones(nlarge,1),0);    %this must be diagonally dominant or else the method won't converge
--- Midterm/tridiag2.m
+++ Midterm/tridiag2.m
@@ -3,33 +3,33 @@
 %%using the Thomas algorithm. Verify your solution by applying it to the iterative test problem for
 %%HW2 in the EP501 Assignments repository
 
-function [Atom] = tridiag(A,b)
+function [Atom] = tridiag2(A,b)
 %forward elimination
-nref=length(b); %system size for reference problem
-Aprime=zeros(nref-1,3);
+nref=length(b);                %system size for reference problem
 Atom=cat(2,A,b); 
+Aprime=zeros(nref,3);
+for i=2:nref-1
+    Aprime(nref,1)=Atom(nref,nref-1);
+    Aprime(nref,2)=Atom(nref,nref);
+end %for    
+Aprime(1,2)=Atom(1,2);
+Aprime(1,3)=Atom(1,3);
+
 for ir1=2:nref
-    em=Atom(ir1,ir1+1)./Atom(ir1-1,ir1);
-    Atom(ir1,ir1)=em;
-    Atom(ir1,1)=Atom(ir1,2)-em.*Atom(ir1-1,3);
+    em=Atom(ir1,1)./Atom(ir1-1,2);
+    Atom(ir1,1)=em;
+    Atom(ir1,2)=Atom(ir1,2)-em.*Atom(ir1-1,3);
     b(ir1)=b(ir1)-Atom(ir1,1).*b(ir1-1);
 end %for
-disp(Atom);
+
 %% Illustrate back substitution on B using provided Matlab function
 x(nref)=b(nref)/Atom(nref,2);
 for ir1=nref-1:-1:1
     x(ir1)=(b(ir1)-Atom(ir1,3)).*x(ir1+1)./Atom(ir1,2);
 end %for
- 
-% for i=1:length(x)
-%     fprintf('\nx%d = %f\n',i,x(i));
-% end %for
 
-disp('Alec Elim solution');
+disp('Elimination/back sub solution:  ');
 disp(x);
-% disp('Dr. Z Elimination/back sub solution:  ');
-% xsoln=backsub(Atom);
-% disp(xsoln);
 
 % disp('Matlab,GNU/Octave built-in solution:  ');
 % disp(A\b);
