a5367221704051f6a87d8a0bab78d5639ba9cd90
Alec Sblendorio <alecsblendorio@Alecs-MacBook-Air.local>
2020-10-19 15:29:48 -0400

    Midterm Problem3b

---
+++ Midterm/polynomial.m
@@ -0,0 +1,9 @@
+%%Problem 3b
+% Write a polynomial division algorithm capable of dividing a given polynomial 
+% Pn(x) (of order n and defined by a set of coefficients) by a given divisor (x − N).
+% I.e. find Qn−1(x) such that: Pn(x) = (x − N)Qn−1(x) + R 
+% Qn−1 is the polynomial left once you divide (x − N) out of Pn. Test your code by using it to
+% divide out a factor of (x − 5) from the polynomial:
+% x^5 −15x^4 +85x^3 −225x^2 +274x−120 = 0
+
+
