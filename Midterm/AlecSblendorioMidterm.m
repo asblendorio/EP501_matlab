289fe867bc7b301b844c889a23f4be6387dd933f
Alec Sblendorio <alecsblendorio@Alecs-MacBook-Air.local>
2020-10-21 20:21:16 -0400

    Midterm Problem3 additions 21 october

--- Midterm/AlecSblendorioMidterm.m
+++ Midterm/AlecSblendorioMidterm.m
@@ -2,6 +2,9 @@
 %% Due Date: October 21, 2020
 %% Subject: EP 501 Numerical Methods for Scientists and Engineers 
 %% MIDTERM EXAM 
+%% Import Data 
+load('iterative_testproblem.mat');
+load('PolynomialTest.mat');
 %% Problem 1 
 % Problem #1: In class we compared the efficiency of Gaussian elimination vs. Jacobi iteration for linear systems of various size in the benchmark scripts 
 % In this problem we will explore a special method for solving tridiagonal systems that is even more efficient than these two options - the Thomas algorithm.
@@ -19,7 +22,7 @@
 % Verify your solution by applying it to the iterative test problem for HW2 in the EP501 Assignments repository 
 % in ./HW2/iterative testproblem.mat file (this problem is actually tridiagonal).
 %Data = importdata('/Users/alecsblendorio/Documents/Projects/EP501_matlab/Midterm/iterative_testproblem.mat');
-load('iterative_testproblem.mat');
+
 disp('%%%%%%%%%%%%%%%%%%PROBLEM #1 ANSWER BEGIN%%%%%%%%%%%%%%%%%%');
 disp('%%%%%%%%Part 1A Solution:%%%%%%%');
 soln = @tridiag; 
@@ -114,12 +117,12 @@ legend('Gauss elim.','Jacobi it.','TriDiag')
 title('Empirically Determined Performance');
 disp('%%%%%%%%End Part 1B Solution:%%%%%%%');
 %% Part C & D
-% Handwritten answer input below.
+% % Handwritten Components are scanned and added in here
 
 disp('%%%%%%%%%%%%%%%%%%PROBLEM #1 ANSWER END%%%%%%%%%%%%%%%%%%');
 %% Problem 2
 disp('%%%%%%%%%%%%%%%%%%PROBLEM #2 ANSWER BEGIN%%%%%%%%%%%%%%%%%%');
-% Handwritten Components are scanned and added in here:
+% Handwritten Components are scanned and added in here
 %% Part A 
 % Under what condition does this homogeneous system of equations 
 % have a nontrivial solution (viz. u = 0)?
@@ -203,6 +206,7 @@ disp('%%%%%%%%%%%%%%%%%%PROBLEM #2 ANSWER END%%%%%%%%%%%%%%%%%%');
 %% Problem 3 
 disp('%%%%%%%%%%%%%%%%%%PROBLEM #3 ANSWER BEGIN%%%%%%%%%%%%%%%%%%');
 %% Part A
+disp('%%%%%%%%Part 3A Solution:%%%%%%%');
 % (a) Write a Python or MATLAB function that analytically solves a quadratic equation of the form:
 % ax2 +bx+c = 0 
 % and compare against roots that you find by hand via factorization or quadratic formula. 
@@ -214,7 +218,10 @@ coef=[2;-6;4];
 f=@quadratic;
 y=f(coef,x);
 disp(y);
-%% Part B
+disp('%%%%%%%%End Part 3A Solution:%%%%%%%');
+%% Part B, D, and E
+disp('%%%%%%%%Part 3B, D, and E Solution:%%%%%%%');
+% Part B Prompt:
 % Write a polynomial division algorithm capable of dividing a given polynomial 
 % Pn(x) (of order n and defined by a set of coefficients) by a given divisor (x − N).
 % I.e. find Qn−1(x) such that: Pn(x) = (x − N)Qn−1(x) + R 
@@ -222,31 +229,27 @@ disp(y);
 % divide out a factor of (x − 5) from the polynomial:
 % x^5 −15x^4 +85x^3 −225x^2 +274x−120 = 0
 
+% Part D Prompt: 
+%Use your synthetic division algorithm to factor the root found in part c out of Eqn. 7 
+%to produce a lower-order polynomial (i.e. Qn−1(x)). 
+%The result will be a set of coefficients for a fourth order polynomial, denoted Q4.
+
+% Part E Prompt:
+% Write a program to repeat the “deflation” process described above (parts c and d) 
+% to find all of roots of Eqn. 7. This can be done by taking Qn-1 
+% You can code your script to work specifically for this fifth order polynomial example
+% it does not have to be general enough to work with a polynomial of arbitrary degree 
 
 
 
+disp('%%%%%%%%Part 3B, D, and E Solution:%%%%%%%');
 %% Part C
+disp('%%%%%%%%Part 3C Solution:%%%%%%%');
 % Use an approximate Newton’s method (using an approximate derivative) to
-% find one root of Eqn. 7
-% x^5 −15x^4 +85x^3 −225x^2 +274x−120 = 0
+% find one root of Eqn. 7: x^5 −15x^4 +85x^3 −225x^2 +274x−120 = 0
 % A script to demonstrate solutions to nonlinear equations on closed
 % and open domains
-%
 % requires:  objfun?.m (set function pointer f to desired function at beginning of program)
-
-
-%% Params for Newton iteration
-maxit=100;       %maximum number of iterations allowed
-minx=0;
-maxx=pi/4;
-tol=1e-9;        %how close to zero we need to get to cease iterations
-
-%% Objective function defs.
-f=@objfun;      %set the function for which we are finding roots, change to illustrate different problems
-fprime=@objfun_deriv;
-x=linspace(minx,maxx,64);   %grid for basic plotting purposes
-ygrid=f(x);
-verbose=true;
 
 %% Plot the function we are finding roots for
 figure(2);
@@ -256,30 +259,24 @@ xlabel('x')
 ylabel('y')
 axis tight;
 
-%% Newton-Rhapson root-finding method
-verbose=true;
-j=0;
-rec = 0;
-finalarray1=[];
-
-for i = 0:1.0:10
-    [xNewton,derivative]=newton_approx(f,i,maxit,tol,verbose);    
-    j=j+1; 
-    finalarray1(j)=xNewton; 
-end
-
-result1=finalarray1(1,2);
-result2=finalarray1(1,3);
-result3=finalarray1(1,4);
-result4=finalarray1(1,5);
-result5=finalarray1(1,6);
-fprintf('The Five Roots of the polynomial are: %d,%d,%d,%d,%d\n',result1,result2,result3,result4,result5);
-
+disp('%%%%%%%%End Part 3B Solution:%%%%%%%');
 %% Part D 
+disp('%%%%%%%%Part 3D Solution:%%%%%%%');
 %Use your synthetic division algorithm to factor the root found in part c out of Eqn. 7 
 %to produce a lower-order polynomial (i.e. Qn−1(x)). 
 %The result will be a set of coefficients for a fourth order polynomial, denoted Q4.
 disp('%%%%%%%%End Part 3D Solution:%%%%%%%');
+
+%% Part E
+disp('%%%%%%%%Part 3E Solution:%%%%%%%');
+% Write a program to repeat the “deflation” process described above (parts c and d) 
+% to find all of roots of Eqn. 7. This can be done by taking Qn-1 
+% You can code your script to work specifically for this fifth order polynomial example
+% it does not have to be general enough to work with a polynomial of arbitrary degree 
+
+
+
+disp('%%%%%%%%End Part 3E Solution:%%%%%%%');
 disp('%%%%%%%%%%%%%%%%%%PROBLEM #3 ANSWER END%%%%%%%%%%%%%%%%%%');
 
 
--- Midterm/polySolver.m
+++ Midterm/polySolver.m
@@ -1,27 +1,26 @@
-% Problem 3d and e
+% Problem 3 B through E 
+% This is an implementation of the Newton-Horner Method that will perform
+% all tasks laid out in problems 3 B through E. 
 % 
 function[r,Pnew]= polySolver(a,r0,tol,nmax)
-nref=length(a)-1;
-r=zeros(nref,1);
-Pnew=zeros(nref,1);
-% r0=0;
-% tol=1e-9;
-% nmax=100;
-for i=1:1:nref
+nref=length(a)-1;  
+r=zeros(nref,1);    % setting up empty array
+Pnew=zeros(nref,1);  % setting up empty array
+for i=1:1:nref      % 
     ni=0;
     x=r0;
     err=10;
-    while ni<nmax && err>=tol 
-        [v,p]=polynomial(a,x);
-        [dv,~]=polynomial(p,x);
+    while ni<nmax && err>=tol % 
+        [v,p]=polynomial(a,x);   % pass in the synthetic polynomial division algorithm to perform factoring 
+        [dv,~]=polynomial(p,x);   % computes the derivative of the polynomial using output 
         xn=x-v/dv;
-        err=abs(xn-x);
+        err=abs(xn-x);              
         ni=ni+1;
         x=xn;
     end %while    
-    r(i)=x;
+    r(i)=x;                 
     Pnew(i)=ni;
-    [~,a]=polynomial(a,x); %deflated
+    [~,a]=polynomial(a,x); %performs deflation for each polynomial 
       
 end %for
 
