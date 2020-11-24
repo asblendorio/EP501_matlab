5b704c73ee5dedcfe8e07b7685cd15ffdef6ee82
Alec Sblendorio <alecsblendorio@Alecs-MacBook-Air.local>
2020-11-24 15:36:17 -0500

    Project5 24 November5 2020

--- differentiation/grad_and_laplacian.m
+++ differentiation/grad_and_laplacian.m
@@ -3,26 +3,25 @@ lx=100;
 ly=100;
 
 %constants
-I=10;
-a=0.005;
-m0=(4*pi)*10.^(-7);
+Q=1;
+a=1;
+e0=8.854.*10.^(-12);
 
-% x=linspace(-5,5,lx);
-% y=linspace(-5,5,ly);
 x = linspace(-3*a,3*a,lx);
 y = linspace(-3*a,3*a,ly);
-
 [X,Y]=meshgrid(x,y);
 %f=exp(-(X.^2)/2/2).*exp(-Y.^2/2/1);
 
 f = zeros(100,100);
 for i=1:100
     for j=1:100
-        if (sqrt(x(i).^2 +y(j).^2) < a)
-            f(i,j) = ((m0.*I)/(2.*pi.*a.^2).*sqrt(x(i).^2+y(j).^2)).*(-y(j)/sqrt(x(i).^2+y(j).^2)+(x(i)./sqrt(x(i).^2+y(j).^2)));
+        for k=1:100
+        if (sqrt(x(i).^2 +y(j).^2+z(k).^2) < a)
+            f(i,j,k) = Q/(4.*pi.*e0.*a) - Q/(8.*pi.*e0.*a.^3).*(x(i).^2+y(j).^2+z(k).^2-a.^2); 
         else
-            f(i,j) = ((m0.*I)/(2.*pi.*sqrt(x(i).^2+y(j).^2))).*(-y(j)/sqrt(x(i).^2+y(j).^2)+(x(i)./sqrt(x(i).^2+y(j).^2)));
+            f(i,j,k) = Q/(4.*pi.*e0.*sqrt(x(i).^2 +y(j).^2+z(k).^2));
         end %if 
+        end %for 
     end %for
 end %for
 
@@ -63,37 +62,6 @@ grady(ly,:)=(f(ly,:)-f(ly-1,:))/dy;
 %add quiver on top of color plot
 hold on;
 quiver(X,Y,gradx,grady,'Color','white','LineWidth',2);
-set(gca,'FontSize',24);
-hold off;
-
-%% Curl 
-curlx=zeros(size(f));
-curly=zeros(size(f));
-
-%x component of curl
-curlx(:,1)=(f(:,2)-f(:,1))/dx;
-for ix=2:lx-1
-    curlx(:,ix)=(f(:,ix+1)-f(:,ix-1))/2/dx;    %\partial/\partial x
-end %for
-curlx(:,lx)=(f(:,lx)-f(:,lx-1))/dx;
-
-%y component of curl 
-curly(1,:)=(f(2,:)-f(1,:))/dy;
-for iy=2:ly-1
-    curly(iy,:)=(f(iy+1,:)-f(iy-1,:))/2/dy;    %\partial/\partial y
-end %for
-curly(ly,:)=(f(ly,:)-f(ly-1,:))/dy;
-
-
-%add quiver for curl on top of color plot
-figure(3);
-contour3(x,y,f);
-xlabel('x');
-ylabel('y');
-
-title('f(x,y) and grad(f)');
-colorbar;
-quiver(X,Y,curlx,curly,'Color','white','LineWidth',2);
 set(gca,'FontSize',24);
 hold off;
 
