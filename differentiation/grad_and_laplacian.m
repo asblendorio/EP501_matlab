65ac357c8240d83cd5e0e95b5db62de834117257
Alec Sblendorio <alecsblendorio@Alecs-MacBook-Air.local>
2020-11-24 15:39:32 -0500

    Project5 24 November6 2020

--- differentiation/grad_and_laplacian.m
+++ differentiation/grad_and_laplacian.m
@@ -1,6 +1,7 @@
 %% Multidimensional function and partial derivatives:  grad and div (laplacian and curl also useful)
 lx=100;
 ly=100;
+lz=100;
 
 %constants
 Q=1;
@@ -9,7 +10,8 @@ e0=8.854.*10.^(-12);
 
 x = linspace(-3*a,3*a,lx);
 y = linspace(-3*a,3*a,ly);
-[X,Y]=meshgrid(x,y);
+z = linspace(-3*a,3*a,lz);
+[X,Y,Z]=meshgrid(x,y,z);
 %f=exp(-(X.^2)/2/2).*exp(-Y.^2/2/1);
 
 f = zeros(100,100);
@@ -40,10 +42,12 @@ colorbar;
 %gradient of scalar function
 dx=x(2)-x(1);
 dy=y(2)-y(1);
+dz=z(2)-z(1);
 
 %% Gradient
 gradx=zeros(size(f));
 grady=zeros(size(f));
+gradz=zeros(size(f));
 
 %x component of gradient
 gradx(:,1)=(f(:,2)-f(:,1))/dx;
@@ -59,15 +63,23 @@ for iy=2:ly-1
 end %for
 grady(ly,:)=(f(ly,:)-f(ly-1,:))/dy;
 
+%z component of gradient
+gradz(1,:)=(f(2,:)-f(1,:))/dz;
+for iz=2:lz-1
+    gradz(iz,:)=(f(iz+1,:)-f(iz-1,:))/2/dz;    %\partial/\partial y
+end %for
+gradz(lz,:)=(f(lz,:)-f(lz-1,:))/dz;
+
 %add quiver on top of color plot
 hold on;
-quiver(X,Y,gradx,grady,'Color','white','LineWidth',2);
+quiver3(X,Y,Z,gradx,grady,gradz,'Color','white','LineWidth',2);
 set(gca,'FontSize',24);
 hold off;
 
 %% Take the Laplacian by taking divergence of the previously computed gradient
 f=gradx;
 g=grady;
+h=gradz;
 
 %x-derivative part of the divergence
 divx=zeros(size(f));
@@ -86,15 +98,15 @@ end %for
 divy(ly,:)=(g(ly,:)-g(ly-1,:))/dy;
 
 
-%z-derivative part of the divergence 
-% divz=zeros(size(z));
-% divz(1,:)=(g(2,:)-g(1,:))/dz;
-% for iz=2:lz-1
-%     divz(iz,:)=(g(iz+1,:)-g(iz-1,:))/2/dz;
-% end %for
-% divz(ly,:)=(g(lz,:)-g(lz-1,:))/dz;
+% z-derivative part of the divergence 
+divz=zeros(size(z));
+divz(1,:)=(h(2,:)-h(1,:))/dz;
+for iz=2:lz-1
+    divz(iz,:)=(h(iz+1,:)-h(iz-1,:))/2/dz;
+end %for
+divz(lz,:)=(h(lz,:)-h(lz-1,:))/dz;
 
-div=divx+divy;    %this is really laplacian b/c input is gradient
+div=divx+divy+divz;    %this is really laplacian b/c input is gradient
 
 figure(4);
 surface(x,y,div);
