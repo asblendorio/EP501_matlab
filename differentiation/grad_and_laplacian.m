b7bd10aa7d97e7d74e8a397247a2f635413e8807
Alec Sblendorio <alecsblendorio@Alecs-MacBook-Air.local>
2020-11-24 16:26:18 -0500

    Project5 24 November7 2020

--- differentiation/grad_and_laplacian.m
+++ differentiation/grad_and_laplacian.m
@@ -1,7 +1,7 @@
 %% Multidimensional function and partial derivatives:  grad and div (laplacian and curl also useful)
 lx=100;
 ly=100;
-lz=100;
+% lz=100;
 
 %constants
 Q=1;
@@ -10,20 +10,18 @@ e0=8.854.*10.^(-12);
 
 x = linspace(-3*a,3*a,lx);
 y = linspace(-3*a,3*a,ly);
-z = linspace(-3*a,3*a,lz);
-[X,Y,Z]=meshgrid(x,y,z);
+% z = linspace(-3*a,3*a,lz);
+[X,Y]=meshgrid(x,y);
 %f=exp(-(X.^2)/2/2).*exp(-Y.^2/2/1);
-
+z=0;
 f = zeros(100,100);
 for i=1:100
     for j=1:100
-        for k=1:100
-        if (sqrt(x(i).^2 +y(j).^2+z(k).^2) < a)
-            f(i,j,k) = Q/(4.*pi.*e0.*a) - Q/(8.*pi.*e0.*a.^3).*(x(i).^2+y(j).^2+z(k).^2-a.^2); 
+        if (sqrt(x(i).^2 +y(j).^2) < a)
+            f(i,j) = Q/(4.*pi.*e0.*a) - Q/(8.*pi.*e0.*a.^3).*(x(i).^2+y(j).^2-a.^2); 
         else
-            f(i,j,k) = Q/(4.*pi.*e0.*sqrt(x(i).^2 +y(j).^2+z(k).^2));
+            f(i,j) = Q/(4.*pi.*e0.*sqrt(x(i).^2 +y(j).^2));
         end %if 
-        end %for 
     end %for
 end %for
 
@@ -42,12 +40,12 @@ colorbar;
 %gradient of scalar function
 dx=x(2)-x(1);
 dy=y(2)-y(1);
-dz=z(2)-z(1);
+% dz=z(2)-z(1);
 
 %% Gradient
 gradx=zeros(size(f));
 grady=zeros(size(f));
-gradz=zeros(size(f));
+% gradz=zeros(size(f));
 
 %x component of gradient
 gradx(:,1)=(f(:,2)-f(:,1))/dx;
@@ -64,22 +62,22 @@ end %for
 grady(ly,:)=(f(ly,:)-f(ly-1,:))/dy;
 
 %z component of gradient
-gradz(1,:)=(f(2,:)-f(1,:))/dz;
-for iz=2:lz-1
-    gradz(iz,:)=(f(iz+1,:)-f(iz-1,:))/2/dz;    %\partial/\partial y
-end %for
-gradz(lz,:)=(f(lz,:)-f(lz-1,:))/dz;
+% gradz(1,:)=(f(2,:)-f(1,:))/dz;
+% for iz=2:lz-1
+%     gradz(iz,:)=(f(iz+1,:)-f(iz-1,:))/2/dz;    %\partial/\partial y
+% end %for
+% gradz(lz,:)=(f(lz,:)-f(lz-1,:))/dz;
 
 %add quiver on top of color plot
 hold on;
-quiver3(X,Y,Z,gradx,grady,gradz,'Color','white','LineWidth',2);
+quiver(X,Y,gradx,grady,'Color','white','LineWidth',2);
 set(gca,'FontSize',24);
 hold off;
 
 %% Take the Laplacian by taking divergence of the previously computed gradient
 f=gradx;
 g=grady;
-h=gradz;
+% h=gradz;
 
 %x-derivative part of the divergence
 divx=zeros(size(f));
@@ -99,14 +97,14 @@ divy(ly,:)=(g(ly,:)-g(ly-1,:))/dy;
 
 
 % z-derivative part of the divergence 
-divz=zeros(size(z));
-divz(1,:)=(h(2,:)-h(1,:))/dz;
-for iz=2:lz-1
-    divz(iz,:)=(h(iz+1,:)-h(iz-1,:))/2/dz;
-end %for
-divz(lz,:)=(h(lz,:)-h(lz-1,:))/dz;
+% divz=zeros(size(z));
+% divz(1,:)=(h(2,:)-h(1,:))/dz;
+% for iz=2:lz-1
+%     divz(iz,:)=(h(iz+1,:)-h(iz-1,:))/2/dz;
+% end %for
+% divz(lz,:)=(h(lz,:)-h(lz-1,:))/dz;
 
-div=divx+divy+divz;    %this is really laplacian b/c input is gradient
+div=divx+divy;    %this is really laplacian b/c input is gradient
 
 figure(4);
 surface(x,y,div);
