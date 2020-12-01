f62001837f161cec8914512cad33bce9fbe5cb0e
Alec Sblendorio <alecsblendorio@Alecs-MacBook-Air.local>
2020-12-01 17:47:39 -0500

    Project5 1 December8 2020

--- differentiation/lineintegration.m
+++ differentiation/lineintegration.m
@@ -11,3 +11,13 @@ x = linspace(-3*a,3*a,lx);
 y = linspace(-3*a,3*a,ly);
 [X,Y]=meshgrid(x,y);
 
+r_phi = zeros(100,100);
+for i=1:100
+    for j=1:100
+        if (sqrt(x(i).^2 +y(j).^2) < a)
+            r_phi(i,j) = ((m0.*I)/(2.*pi.*a.^2).*sqrt(x(i).^2+y(j).^2)).*(-y(j)/sqrt(x(i).^2+y(j).^2)+(x(i)./sqrt(x(i).^2+y(j).^2)));
+        else
+            r_phi(i,j) = ((m0.*I)/(2.*pi.*sqrt(x(i).^2+y(j).^2))).*(-y(j)/sqrt(x(i).^2+y(j).^2)+(x(i)./sqrt(x(i).^2+y(j).^2)));
+        end %if 
+    end %for
+end %for
