63ef7de86558227a5fb2256e40816d373b6ea694
Alec Sblendorio <alecsblendorio@Alecs-MacBook-Air.local>
2020-12-09 01:35:04 -0500

    Final Exam Update13

--- AlecSblendorio_FinalExam/finite_diff.m
+++ AlecSblendorio_FinalExam/finite_diff.m
@@ -2,6 +2,7 @@
 % Analytical Plotter 
 lx=100;
 x=linspace(0,2*pi,lx)';
+dx=x(2)-x(1);
 y=cos(x);
 dy=-sin(x);
 ddy=-cos(x);
@@ -21,62 +22,27 @@ ylabel('y(x) or y''(x)');
 title('Analytically Solved Functions');
 
 %% Comparison of basic numerical derivative
-%second order, centered
-dy_dx=zeros(lx,1);
-dx=x(2)-x(1);
-
-%forward difference at the beginning
-dy_dx(1)=(y(2)-y(1))/dx;
-
-%centered difference on the interior
-for ix=2:lx-1
-    dy_dx(ix)=(y(ix+1)-y(ix-1))/2/dx;
-end %for
-
-%backward difference at the end
-dy_dx(lx)=(y(lx)-y(lx-1))/dx;
-
-% plot(x,dy_dx,'k--')
-
-%first order derivative approximation (backward)
+%first,second,third order derivative approximation (backward)
 %interior
-dy_dxbwd1=zeros(lx,1);
-dy_dxbwd2=zeros(lx,1);
-dy_dxbwd3=zeros(lx,1);
+dx1=zeros(lx,1);
+dx2=zeros(lx,1);
+dx3=zeros(lx,1);
 
 for ix=2:lx
-        dy_dxbwd1(ix)=(y(ix)-y(ix-1))/dx;
-        dy_dxbwd2(ix)=(y(ix)-2.*y(ix-1)+y(ix-2))/2*dx;
-        dy_dxbwd3(ix)=(y(ix-2)-6.*y(ix-1)+3.*y(ix)+2.*y(ix+1))/6*dx;
+        dx1(ix)=(y(ix)-y(ix-1))/dx;
+        dx2(ix)=(y(ix)-2.*y(ix-1)+y(ix-2))/2/dx;
+        dx3(ix)=(y(ix-2)-6.*y(ix-1)+3.*y(ix)+2.*y(ix+1))/6*dx;
 end %for
-dy_dxbwd1(1)=dy_dxbwd1(2);
-dy_dxbwd2(1)=dy_dxbwd2(2);
-dy_dxbwd3(1)=dy_dxbwd3(2);
+dx1(1)=dx1(2);
+dx2(1)=dx2(2);
+dx3(1)=dx3(2);
 
-% %second order derivative approximation (backward)
-% %interior
-% for ix=2:lx
-%     dy_dxbwd(ix)=(y(ix)-2.*y(ix-1)+y(ix-2)/2*dx;
-% end %for
-% dy_dxbwd(1)=dy_dxbwd(2);
-% 
-% %third order derivative approximation (backward)
-% for ix=2:lx
-%     dy_dxbwd(ix)=(y(ix-2)-6.*y(ix-1)+3.*y(ix)+2.*y(ix+1))/6*dx;
-% end %for
-% dy_dxbwd(1)=dy_dxbwd(2);
-
-% plot(x,dy_dxbwd,'m--')
-% legend('original function','analytical','centered','backward')
-% xlabel('x');
-% ylabel('y(x) or y''(x)');
-% title('Comparison of finite difference derivative approximations');
 figure(2);
-plot(x,dy_dxbwd1,'m--')
+plot(x,dx1,'m--')
 hold on;
-plot(x,dy_dxbwd2,'b--')
+plot(x,dx2,'b--')
 hold on;
-plot(x,dy_dxbwd3,'k--')
+plot(x,dx3,'k--')
 
 legend('original function','analytical','centered','backward')
 xlabel('x');
