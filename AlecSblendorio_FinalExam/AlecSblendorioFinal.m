63ab2d9787c25dae3ac6b49a80b8489b380fc9f1
Alec Sblendorio <alecsblendorio@Alecs-MacBook-Air.local>
2020-12-10 11:30:54 -0500

    Problem 2 Update1

--- AlecSblendorio_FinalExam/AlecSblendorioFinal.m
+++ AlecSblendorio_FinalExam/AlecSblendorioFinal.m
@@ -389,17 +389,20 @@ dx3=zeros(lx1,1);
 dx4=zeros(lx1,1);
 
 for ix=3:lx-2
-        dx1(ix)=(y(ix)-y(ix-1))./dx;
-        dx2(ix)=(y(ix)-2.*y(ix-1)+y(ix-2))/2.*dx;
-        dx3(ix)=(-2.*y(ix-1)-3.*y(ix)+6.*y(ix+1)-y(ix+2))/6.*dx;
-        dx4(ix)=(y(ix-2)-8.*y(ix-1)+8.*y(ix+1)-y(ix+2))/12*dx;
+        dx1(ix)=(y(ix)-y(ix-1))/(dx);
+        dx2(ix)=(y(ix+1)-2*y(ix)+y(ix-1))/(dx^2);
+        dx3(ix)=(-2*y(ix-1)-3*y(ix)+6*y(ix+1)-y(ix+2))/(6*dx);
+        dx4(ix)=(y(ix-2)-8*y(ix-1)+8*y(ix+1)-y(ix+2))/(12*dx);
 end %for
+
 % dx1(1)=dx1(2);
 % dx2(1)=dx2(2);
 % dx3(1)=dx3(2);
 % dx4(1)=dx4(2);
 
 figure(11);
+plot(x,y,'y--')
+hold on;
 plot(x,dx1,'m--')
 hold on;
 plot(x,dx2,'b--')
@@ -408,7 +411,7 @@ plot(x,dx3,'k--')
 hold on;
 plot(x,dx4,'r--')
 
-legend('1st derivative','2nd derivative','3rd derivative','4th derivative');
+legend('Original Function','1st derivative','2nd derivative','3rd derivative','4th derivative');
 xlabel('x');
 ylabel('y(x) or y''(x)');
 title('Numerically Solved Functions');
