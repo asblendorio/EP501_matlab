30087d793825dddff5d77912a24f03a0f3e0375e
Alec Sblendorio <alecsblendorio@Alecs-MacBook-Air.local>
2020-12-11 00:01:13 -0500

    Problem 2 Update2

--- AlecSblendorio_FinalExam/LUinv.m
+++ AlecSblendorio_FinalExam/LUinv.m
@@ -6,7 +6,7 @@ invA=zeros(nref2,nref2);
 k = eye(nref2);
 
 for ir1=1:nref1
-    b(1,ir1)=k(1,ir1)./L(1,1);
+    b(1,ir1)=k(1,ir1)/L(1,1);
     for ir2=2:nref1
         j=0;
         for ir3=ir2-1:-1:1
@@ -17,13 +17,13 @@ for ir1=1:nref1
 end %for
 
 for ir1=1:nref1
-    invA(nref1,ir1)=k(nref1,ir1)./U(nref1,nref1);
+    invA(nref1,ir1)=k(nref1,ir1)/U(nref1,nref1);
     for ir2=nref1-1:-1:1
         j=0;
         for ir3=ir2+1:nref1
-            j=j+U(ir2,ir3).*invA(ir3,ir1);
+            j=j+U(ir2,ir3)*invA(ir3,ir1);
         end %for
-        invA(ir2,ir1)=b(ir2,ir1)-j./U(ir2,ir2);
+        invA(ir2,ir1)=b(ir2,ir1)-j/U(ir2,ir2);
     end %for
 end %for
    
